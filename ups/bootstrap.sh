5a41c9a241998f11c805fdb026f638595a811985
Lynn Garren <garren@fnal.gov>
2013-11-26 00:10:45 -0600

    trying to fix bootstrap

--- ups/bootstrap.sh
+++ ups/bootstrap.sh
@@ -30,9 +30,24 @@ then
 fi
 
 package=mrb
-pkgver=v0_03_02
+pkgver=v0_03_03
 
 get_my_dir
+
+pkgdir=${product_dir}/${package}
+if [ ! -d ${pkgdir} ]
+then
+  mkdir -p ${pkgdir} || exit 2;
+fi
+
+set -x
+# pull the tagged release from git
+git archive --prefix=${pkgver}/ \
+            --remote ssh://p-${package}@cdcvs.fnal.gov/cvs/projects/${package} \
+            -o ${mydir}/${package}-${pkgver}.tar ${pkgver}
+cd ${pkgdir}
+tar xf ${mydir}/${package}-${pkgver}.tar
+set +x
 
 # we will use ups declare
 if [ -z ${UPS_DIR} ]
@@ -41,19 +56,6 @@ then
    exit 1
 fi
 source `${UPS_DIR}/bin/ups setup ${SETUP_UPS}`
-
-pkgdir=${product_dir}/${package}
-if [ ! -d ${pkgdir} ]
-then
-  mkdir -p ${pkgdir} || exit 2;
-fi
-
-# pull the tagged release from git
-git archive --prefix=${pkgver}/ \
-            --remote ssh://p-${package}@cdcvs.fnal.gov/cvs/projects/${package} \
-            -o ${mydir}/${package}-${pkgver}.tar ${pkgver}
-cd ${pkgdir}
-tar xf ${mydir}/${package}-${pkgver}.tar
 
 ups declare -c ${package} ${pkgver} -r ${package}/${pkgver} -0 -m ${package}.table  -z ${product_dir}
 
