b673d5d39c7f6c3b48bba552d56f7ad624395db8
Chris Green <greenc@fnal.gov>
2021-01-27 17:58:17 -0600

    mrb uv now works correctly with cetmodules packages.

--- libexec/updateDepsPV.sh
+++ libexec/updateDepsPV.sh
@@ -32,17 +32,17 @@ function modify_product_deps()
   pdfile=${pkg}/ups/product_deps
   pkg_name="$(sed -Ene 's&^[[:space:]]*parent[[:space:]]+([^[:space:]\#]+).*&\1&p; T; q' "${pdfile}")"
 
-  echo "updating dependencies for ${pkg_name} in ${pdfile}"
-  $MRB_DIR/libexec/edit_product_deps ${pdfile} ${product} ${new_version} ${dryRun}  || exit 1
+  echo "INFO: updating dependencies for ${pkg_name} in ${pdfile}"
+  $MRB_DIR/libexec/edit_product_deps ${pdfile} ${product} ${new_version} ${dryRun}
 }
 
 function modify_cmake()
 {
   local cfile="$1"
-  echo "editing $cfile"
-  grep -Ee '\b'"${product}"'\b' "${cfile}"
-  $MRB_DIR/libexec/edit_cmake "${cfile}" ${product} ${new_version} ${dryRun}  || exit 1
-  grep -Ee '\b'"${product}"'\b' "${cfile}"
+  if grep -Ee '\b'"${product}"'\b' "${cfile}" >/dev/null 2>&1; then
+    echo "INFO: editing $cfile"
+    $MRB_DIR/libexec/edit_cmake "${cfile}" ${product} ${new_version} ${dryRun}
+  fi
 }
 
 function get_package_list()
@@ -68,59 +68,85 @@ while getopts ":hdRp:" OPTION
 do
     case $OPTION in
         h   ) usage ; exit 0 ;;
-        d   ) dryRun="yes" ; echo 'DRY RUN - Changes are not saved';;
+        d   ) dryRun="yes" ; echo 'DRY RUN: changes are not saved';;
         R   ) restore="yes" ;; 
         *   ) echo "ERROR: Unknown option" ; usage ; exit 1 ;;
     esac
 done
 
+failed=()
 if [ $restore == "yes" ]; then
+  operation="restore"
   get_package_list
   for d in "${packages[@]}"; do
     # Sanity checks
-    if [ ! -r "$d/ups/product_deps" ]; then
-      echo "Cannot find ups/product_deps in $d"
-      break
+    if [ -d "$d/.git" ]; then
+      echo "INFO: restoring $d"
+      if pushd "$d" > /dev/null; then
+        git checkout -qf -- ups/product_deps || \
+          {
+          echo "WARNING: unable to restore ups/product_deps for $d" 1>&2
+          failed+=($d)
+        }
+        popd > /dev/null
+      else
+        echo "WARNING: unable to change directory to $d to restore ups/product_deps" 1>&2
+        failed+=($d)
+      fi
+    else
+      echo "WARNING: unable to restore ups/product_deps from non-git repository $d" 1>&2
+      failed+=($d)
     fi
-    echo "Restoring $d"
-    pushd "$d" > /dev/null
-    git checkout -qf -- ups/product_deps
-    popd > /dev/null
   done
-  exit 0
-fi
-
-# Did the user provide a product name?
-shift $((OPTIND - 1))
-if [ $# -lt 1 ]; then
-    echo "ERROR: No product given"
-    usage
+else
+  operation="update"
+  # Did the user provide a product name?
+  shift $((OPTIND - 1))
+  if [ $# -lt 1 ]; then
+    echo "ERROR: no product given" 1>&2
+    usage 1>&2
     exit 1
-fi
+  fi
 
-# Capture the product name
-product=$1
+  # Capture the product name
+  product=$1
 
-# check for version
-if [ $# -lt 2 ]; then
-    echo "ERROR: No version given"
-    usage
+  # check for version
+  if [ $# -lt 2 ]; then
+    echo "ERROR: no version given" 1>&2
+    usage 1>&2
     exit 1
+  fi
+  new_version=$2
+
+  get_package_list
+
+  for d in "${packages[@]}"; do
+    # Sanity checks
+    if [ ! -r "${d}"/CMakeLists.txt ]; then
+      echo "WARNING: cannot find CMakeLists.txt in ${d}" 1>&2
+      false
+    elif [ ! -r "${d}"/ups/product_deps ]; then
+      echo "WARNING: cannot find ups/product_deps in ${d}" 1>&2
+      false
+    else
+      modify_product_deps "${d}" && \
+        modify_cmake "${d}/CMakeLists.txt" && \
+        { if [ -r "${d}"/releaseDB/CMakeLists.txt ]; then modify_cmake "${d}/releaseDB/CMakeLists.txt"; fi; } && \
+        { if [ -r "${d}"/bundle/CMakeLists.txt ]; then modify_cmake "${d}/bundle/CMakeLists.txt"; fi; }
+    fi
+    (( $? == 0 )) || failed+=($d)
+  done
 fi
-new_version=$2
-
-get_package_list
-
-for d in "${packages[@]}"; do
-  # Sanity checks
-  if [ ! -r "${d}"/CMakeLists.txt ]; then echo "Cannot find CMakeLists.txt in ${d}"; break; fi
-  if [ ! -r "${d}"/ups/product_deps ]; then echo "Cannot find ups/product_deps in ${d}"; break; fi
-  modify_product_deps "${d}"
-  if [ -r "${d}"/releaseDB/CMakeLists.txt ]; then modify_cmake "${d}/releaseDB/CMakeLists.txt"; fi
-  if [ -r "${d}"/bundle/CMakeLists.txt ]; then modify_cmake "${d}/bundle/CMakeLists.txt"; fi
-done
 
 echo
+if [ -n "${failed[*]}" ]; then
+  echo "ERROR: $operation failed for packages:\n         ${failed[*]}" 1>&2
+  status=1
+else
+  status=0
+fi
+
 if [ "${dryRun}" = "yes" ]; then
   echo "If the dry run was successful, run: "
   echo " mrb uv ${product} ${new_version}"
@@ -128,4 +154,4 @@ else
   echo 'Be sure to re-run mrbsetenv'
 fi
 
-exit 0
+exit $status
